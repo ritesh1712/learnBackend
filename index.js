35ce7b1a9e53979a76512f27f24c0345b8e66e24
ritesh1712 <105135021+ritesh1712@users.noreply.github.com>
2025-05-11 14:53:55 +0530

    test

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const app = express()
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+app.get('/login', (req, res) => {
+  res.json({name:'ritesh raghuwanshi'})
+})
 
 app.listen(process.env.PORT, () => {
   console.log(`app listening on port ${process.env.PORT}`)
